ffff48447f23db6f2f6d62dd2a73a0cc8f99ffda
Kyle Canoy <kcanoy@uw.edu>
2021-05-04 19:31:46 -0700

    pixabay fetch function w/ possible jquery function as backup

---
+++ assets/pixabayAPI.js
@@ -0,0 +1,28 @@
+var API_KEY = "21425895-73e3d9c4c296584d1ba63965b";
+
+var requestUrl = 
+  "https://pixabay.com/api/?key=" +
+  API_KEY +
+  "&q=" +
+  encodeURIComponent("food+chicken+pasta");
+fetch(requestUrl)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    // for (var i = 0; i < data.length; i++) {
+    //   // var recipePic = document.getElementById("recipePic")
+    //   // recipePic.src = data.largeImageURL
+    // }
+  });
+
+// jquery function that we could possibly use instead
+//   var API_KEY = '21425895-73e3d9c4c296584d1ba63965b';
+// var URL = "https://pixabay.com/api/?key="+API_KEY+"&q="+encodeURIComponent('food chicken pasta');
+// $.getJSON(URL, function(data){
+// if (parseInt(data.totalHits) > 0)
+//     $.each(data.hits, function(i, hit){ console.log(hit.pageURL); });
+// else
+//     console.log('No hits');
+// });
