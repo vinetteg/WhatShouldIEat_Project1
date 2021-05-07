e07dd44188dc321cd38b0931a6364a114706ac2d
Kyle Canoy <kcanoy@uw.edu>
2021-05-06 21:47:36 -0700

    fetch mealdb API and append recipeName and recipeInstructions to recipe page

--- assets/fetch.js
+++ assets/fetch.js
@@ -1,16 +1,65 @@
-//fetch to get image from mealDB
-var getRecipe = function (recipe) {
-  var apiMeal = "https://www.themealdb.com/api/json/v1/1/filter.php?i=beef";
+//fetch to get recipe from mealDB
+// Chicken 
+chickenBtn.addEventListener("click", function() {
+  var apiMeal = "https://www.themealdb.com/api/json/v1/1/search.php?s="+randomFood[0].chicken[randomChickenNum];
+
+var recipeHeader = document.getElementById("recipeHeader")
+var recipeInstructions = document.getElementById("ingredients")
 
   fetch(apiMeal).then(function (response) {
     response.json().then(function (data) {
-      displayRecipe(data);
+      console.log(data);   
+      recipeHeader.innerHTML = data.meals[0].strMeal
+      recipeInstructions.innerHTML = data.meals[0].strInstructions
     });
   });
-};
+})
 
-var recipeContainerEl = document.querySelector("#recipeContainer");
+// Beef
+beefBtn.addEventListener("click", function() {
+  var apiMeal = "https://www.themealdb.com/api/json/v1/1/search.php?s="+randomFood[0].beef[randomBeefNum];
 
-var displayRecipe = function (showRecipe) {
-  recipeContainerEl.textContent = "";
-};
+var recipeHeader = document.getElementById("recipeHeader")
+var recipeInstructions = document.getElementById("ingredients")
+
+  fetch(apiMeal).then(function (response) {
+    response.json().then(function (data) {
+      console.log(data);
+      recipeHeader.innerHTML = data.meals[0].strMeal
+      recipeInstructions.innerHTML = data.meals[0].strInstructions
+    });
+  });
+})
+
+// Veggie
+veggiesBtn.addEventListener("click", function() {
+  var apiMeal = "https://www.themealdb.com/api/json/v1/1/search.php?s="+randomFood[0].chicken[randomVeggieNum];
+
+var recipeHeader = document.getElementById("recipeHeader")
+var recipeInstructions = document.getElementById("ingredients")
+
+  fetch(apiMeal).then(function (response) {
+    response.json().then(function (data) {
+      console.log(data);
+      recipeHeader.innerHTML = data.meals[0].strMeal
+      recipeInstructions.innerHTML = data.meals[0].strInstructions
+    });
+  });
+})
+
+// Fish
+fishBtn.addEventListener("click", function() {
+  var apiMeal = "https://www.themealdb.com/api/json/v1/1/search.php?s="+randomFood[0].fish[randomFishNum];
+
+var recipeHeader = document.getElementById("recipeHeader")
+var recipeInstructions = document.getElementById("ingredients")
+
+  fetch(apiMeal).then(function (response) {
+    response.json().then(function (data) {
+      console.log(data);
+      recipeHeader.innerHTML = data.meals[0].strMeal
+      recipeInstructions.innerHTML = data.meals[0].strInstructions
+    });
+  });
+})
+
