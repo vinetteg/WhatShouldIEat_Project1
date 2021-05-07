8a5135594a745212aba0f1f4a6a3e62a0d3c66f0
Selma Burke <selmasburke@gmail.com>
2021-05-06 19:12:48 -0700

    fixed errrors in html & js, buttons now functional

--- assets/script.js
+++ assets/script.js
@@ -9,43 +9,42 @@ var headerEl = document.querySelector(".header");
 var landingContainer = document.querySelector(".landingContainer");
 var recipeArea = document.querySelector(".recipeArea");
 var foodHistory = document.querySelector(".foodHistory");
+
 function beefRecipes(){
     headerEl.style.display="none";
     landingContainer.style.display="none";
     recipeArea.style.display="block";
 }
-beefBtn.addEventListener("click", randomBeefOption);
 beefBtn.addEventListener("click", beefRecipes);
+
 function chickenRecipes(){
     headerEl.style.display="none";
     landingContainer.style.display="none";
     recipeArea.style.display="block";
 }
-chickenBtn.addEventListener("click", randomChickenOption);
 chickenBtn.addEventListener("click", chickenRecipes);
+
 function fishRecipes(){
     headerEl.style.display="none";
     landingContainer.style.display="none";
     recipeArea.style.display="block";
     console.log(randomFishNum)
 }
-fishBtn.addEventListener("click", randomFishOption);
 fishBtn.addEventListener("click", fishRecipes);
+
 function veggieRecipes(){
     headerEl.style.display="none";
     landingContainer.style.display="none";
     recipeArea.style.display="block";
 }
-veggieBtn.addEventListener("click", randomVeggieOption);
 veggiesBtn.addEventListener("click", veggieRecipes);
+
 function showHistory(){
     recipeArea.style.display="none";
     foodHistory.style.display="block";
 }
 historyBtn.addEventListener("click", showHistory);
-//PENDING APIs
-// function reRoll(){}
-// reRollBtn.addEventListener("click",);
+
 function returnHome(){
     foodHistory.style.display="none";
     headerEl.style.display="block";
