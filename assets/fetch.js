d2b2786c120a8a127a2836ce86a2630e31069a88
Kyle Canoy <kcanoy@uw.edu>
2021-05-06 23:12:59 -0700

    adds local storage to mealdb fetch

--- assets/fetch.js
+++ assets/fetch.js
@@ -7,12 +7,15 @@ function chicken() {
 
 var recipeHeader = document.getElementById("recipeHeader")
 var recipeInstructions = document.getElementById("ingredients")
+var foodStorage = document.getElementById("foodStorage")
 
   fetch(apiMeal).then(function (response) {
     response.json().then(function (data) {
       console.log(data);   
       recipeHeader.innerHTML = data.meals[0].strMeal
       recipeInstructions.innerHTML = data.meals[0].strInstructions
+      localStorage.setItem("recipeName", data.meals[0].strMeal)
+      foodStorage.innerHTML = localStorage.getItem("recipeName")
     });
   });
 }
@@ -20,14 +23,18 @@ var recipeInstructions = document.getElementById("ingredients")
 // Beef
 function beef() {
   var apiMeal = "https://www.themealdb.com/api/json/v1/1/search.php?s="+randomFood[0].beef[randomBeefNum];
+
 var recipeHeader = document.getElementById("recipeHeader");
 var recipeInstructions = document.getElementById("ingredients");
+var foodStorage = document.getElementById("foodStorage")
 
   fetch(apiMeal).then(function (response) {
     response.json().then(function (data) {
       console.log(data);
       recipeHeader.innerHTML = data.meals[0].strMeal
       recipeInstructions.innerHTML = data.meals[0].strInstructions
+      localStorage.setItem("recipeName", data.meals[0].strMeal)
+      foodStorage.innerHTML = localStorage.getItem("recipeName")
     });
   });
 }
@@ -38,12 +45,15 @@ function veggie() {
 
 var recipeHeader = document.getElementById("recipeHeader")
 var recipeInstructions = document.getElementById("ingredients")
+var foodStorage = document.getElementById("foodStorage")
 
   fetch(apiMeal).then(function (response) {
     response.json().then(function (data) {
       console.log(data);
       recipeHeader.innerHTML = data.meals[0].strMeal
       recipeInstructions.innerHTML = data.meals[0].strInstructions
+      localStorage.setItem("recipeName", data.meals[0].strMeal)
+      foodStorage.innerHTML = localStorage.getItem("recipeName")
     });
   });
 }
@@ -54,12 +64,15 @@ function fish() {
 
 var recipeHeader = document.getElementById("recipeHeader")
 var recipeInstructions = document.getElementById("ingredients")
+var foodStorage = document.getElementById("foodStorage")
 
   fetch(apiMeal).then(function (response) {
     response.json().then(function (data) {
       console.log(data);
       recipeHeader.innerHTML = data.meals[0].strMeal
       recipeInstructions.innerHTML = data.meals[0].strInstructions
+      localStorage.setItem("recipeName", data.meals[0].strMeal)
+      foodStorage.innerHTML = localStorage.getItem("recipeName")
     });
   });
 }
