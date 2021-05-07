8a304240b2b9147c3622df3c5b4dd42a7b799c76
Selma Burke <selmasburke@gmail.com>
2021-05-06 22:12:15 -0700

    updated changes

--- assets/fetch.js
+++ assets/fetch.js
@@ -1,10 +1,4 @@
 
-//fetch to get image from mealDB
-
-function getRecipeChicken() {
-var getRecipe = function (recipe) {
-  var apiMeal = "www.themealdb.com/api/json/v1/1/search.php?s=" + "randomFood[0].chicken[randomChickenNum])";
-
 //fetch to get recipe from mealDB
 // Chicken 
 chickenBtn.addEventListener("click", function() {
@@ -25,13 +19,10 @@ var recipeInstructions = document.getElementById("ingredients")
 // Beef
 beefBtn.addEventListener("click", function() {
   var apiMeal = "https://www.themealdb.com/api/json/v1/1/search.php?s="+randomFood[0].beef[randomBeefNum];
+var recipeHeader = document.getElementById("recipeHeader");
+var recipeInstructions = document.getElementById("ingredients");
 
-var recipeHeader = document.getElementById("recipeHeader")
-var recipeInstructions = document.getElementById("ingredients")
 
-
-}
-var recipeContainerEl = document.querySelector("#recipeContainer");
 
   fetch(apiMeal).then(function (response) {
     response.json().then(function (data) {
@@ -73,4 +64,3 @@ var recipeInstructions = document.getElementById("ingredients")
     });
   });
 })
-
